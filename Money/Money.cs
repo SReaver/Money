0d084971b8e56f3f7be743a9ced666fb0a7c32af
плотниковс <@403_5>
2017-05-30 21:58:54 +0600

    Add project files.

---
+++ Money/Money.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    class Money
+    {
+        public int amount;
+        public string unit;
+
+        public Money(int Amount, string Unit)
+        {
+            amount = Amount;
+            unit = Unit;
+
+        }
+
+        public static Money operator + (Money a, Money b)
+        {
+            Money temp = new Money(a.amount + b.amount, a.unit);
+            return temp;
+        }
+        public static bool operator <(Money a, Money b)
+        {
+            if (a.amount<b.amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >(Money a, Money b)
+        {
+            if (a.amount > b.amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Money operator ++(Money a)
+        {
+            a.amount++;
+            return a;
+        }
+
+        public static implicit operator Money(int val)
+        {
+            return new Money(val, "");
+        }
+        public static implicit operator Int32(Money m)
+        {
+            return m.amount;
+        }
+        public static implicit operator Money(string val)
+        {
+            return new Money(0, val);
+        }
+        public static explicit operator string(Money m)
+        {
+            return m.unit;
+        }
+
+    }
+}
---
+++ Money/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Money usd1 = new Money(1, "USD");
+            Money usd2 = new Money(3, "USD");
+            usd1++;
+            Console.WriteLine(usd1.amount + " "+usd1.unit);
+
+        
+
+            if (usd1<usd2)
+            {
+                Console.WriteLine("usd1 Больше");
+            }
+
+            Money m1 = 56; //используя implicit
+            int i = m1; //преобразование в другой тип
+            Money ms = "USD";
+            string s = ms;
+        
+        }
+    }
+}
